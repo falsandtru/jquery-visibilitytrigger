edd31b9ea448a2106082b00ee748f5c14563173d
falsandtru <falsandtru>
2013-01-16 14:28:36 +0900

    update

--- jquery.scrolltrigger.js
+++ jquery.scrolltrigger.js
@@ -5,7 +5,7 @@
  * ---
  * @Copyright(c) 2012, falsandtru
  * @license MIT  http://opensource.org/licenses/mit-license.php  http://sourceforge.jp/projects/opensource/wiki/licenses%2FMIT_license
- * @version 1.0.8
+ * @version 1.0.9
  * @updated 2013/01/16
  * @author falsandtru  http://fat.main.jp/  http://sa-kusaku.sakura.ne.jp/
  * ---
@@ -52,41 +52,57 @@
 		
 		if( settings.reset )
 		{
-			settings.ns = [ settings.gns + ( settings.ns === undefined ? '' : ':' + settings.ns ) ].join( '.' ) ;
-			settings.scope = this.get( 0 ) === window ? document : this ;
-			settings.count = 0 ;
-			settings.reset = false ;
+			jQuery.extend
+			(
+				settings ,
+				{
+					nss :
+					{
+						scroll : [ 'scroll' , settings.gns + ( settings.ns === undefined ? '' : ':' + settings.ns ) ].join( '.' ) ,
+						scrolltrigger :  settings.gns + ( settings.ns === undefined ? '' : '.' + settings.ns ) ,
+						data : settings.gns + ( settings.ns === undefined ? '' : '.' + settings.ns )
+					} ,
+					scope : this.get( 0 ) === window ? document : this ,
+					count : 0 ,
+					reset : false
+				}
+			)
 		}
 		
-		if( !jQuery( settings.scope ).length ){ return this ; }
+		if( !jQuery( settings.scope ).length || !arguments.length ){ return this ; }
 		
 		for( var i = 0 ; i < jQuery( settings.scope ).length ; i++ )
 		{
-			jQuery.data( jQuery( settings.scope ).eq( i ).get( 0 ) , settings.ns , jQuery.extend( {} , settings) ) ;
+			jQuery.data( jQuery( settings.scope ).eq( i ).get( 0 ) , settings.nss.data , jQuery.extend( {} , settings) ) ;
 		}
-		
-		jQuery( this )
-		.bind( [ 'scroll' , settings.ns ].join( '.' ) , settings , function( event )
-		{
-			jQuery( this ).trigger( settings.ns ) ;
-		} );
 		
 		if( settings.expand && this.get( 0 ) !== window )
 		{
-			jQuery( window ).bind( [ 'scroll' , settings.ns ].join( '.' ) , settings , function( event )
+			jQuery( window )
+			.unbind( settings.nss.scroll )
+			.bind( settings.nss.scroll , settings , function( event )
 			{
-				event.data.scope === document ? jQuery( window ).trigger( event.data.ns ) : jQuery( event.data.scope ).trigger( event.data.ns ) ;
+				event.data.scope === document ? null : jQuery( event.data.scope ).trigger( event.data.nss.scrolltrigger ) ;
 			} ) ;
 		}
 		
 		jQuery( this )
-		.bind( settings.ns , settings , function( event )
+		.unbind( settings.nss.scroll )
+		.bind( settings.nss.scroll , settings , function( event )
+		{
+			jQuery( this ).trigger( event.data.nss.scrolltrigger ) ;
+		} )
+		.unbind( settings.nss.scrolltrigger )
+		.bind( settings.nss.scrolltrigger , settings , function( event )
 		{
 			var
 			fire ,
 			area = this === window ? document : this ,
-			settings = jQuery.data( area , event.data.ns ) ,
-			targets = jQuery( settings.trigger , area ) ,
+			settings = jQuery.data( area , event.data.nss.data ) ;
+			
+			if( settings === undefined ){ return this; }
+			
+			targets = jQuery( settings.trigger , area ) ;
 			target = targets.eq( settings.count ) ;
 			
 			if( !target.length )
@@ -96,14 +112,14 @@
 					var
 					remainder = 0 ;
 					
-					jQuery( this ).unbind( [ 'scroll' , settings.ns ].join( '.' ) ).unbind( settings.ns ) ;
-					jQuery.removeData( area , settings.ns ) ;
+					jQuery( this ).unbind( settings.nss.scrolltrigger ).unbind( settings.nss.scroll ) ;
+					jQuery.removeData( area , settings.nss.data ) ;
 					
 					for( var i = 0 ; i < jQuery( settings.scope ).length ; i++ )
 					{
-						remainder += ( undefined === jQuery.data( jQuery( settings.scope ).eq( i ).get( 0 ) , settings.ns ) ? 0 : 1 ) ;
+						remainder += ( undefined === jQuery.data( jQuery( settings.scope ).eq( i ).get( 0 ) , settings.nss.data ) ? 0 : 1 ) ;
 					}
-					remainder ? null : jQuery( window ).unbind( [ 'scroll' , settings.ns ].join( '.' ) ) ;
+					remainder ? null : jQuery( window ).unbind( settings.nss.scroll ) ;
 				}
 				return this ;
 			}
@@ -115,7 +131,7 @@
 			settings.callback.apply( target , settings.parameter ) ;
 			
 			settings.count = settings.count + 1 ;
-			jQuery.data( area , settings.ns , settings ) ;
+			jQuery.data( area , settings.nss.data , settings ) ;
 			
 			return arguments.callee.apply( this , arguments ) ;
 			
